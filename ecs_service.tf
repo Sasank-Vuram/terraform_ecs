a62b61cdf6bbef1b2601564f356d6343e7084b85
Sasank Irugu <82870337+Sasank-Vuram@users.noreply.github.com>
2021-04-26 19:32:35 +0530

    Update ecs_service.tf

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,25 +1,41 @@
-resource "aws_ecs_service" "tfer--terraform_ecs_terraform_ecs-002D-service" {
-  cluster = "default"
+resource "aws_codedeploy_app" "terraform_ecs" {
+  compute_platform = "ECS"
+  name             = "default"
+}
 
-  deployment_circuit_breaker {
-    enable   = "false"
-    rollback = "false"
+resource "aws_codedeploy_deployment_group" "terraform_ecs_workhall" {
+  app_name               = aws_codedeploy_app.terraform_ecs.workhall
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+  deployment_group_name  = "terraform_ecs_workhall"
+  service_role_arn       = arn:aws:iam::077286633283:role/aws-code-deploy-terraform
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
   }
 
-  deployment_controller {
-    type = "ECS"
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
   }
 
-  deployment_maximum_percent         = "200"
-  deployment_minimum_healthy_percent = "100"
-  desired_count                      = "1"
-  enable_ecs_managed_tags            = "false"
-  enable_execute_command             = "false"
-  health_check_grace_period_seconds  = "0"
-  launch_type                        = "FARGATE"
-  name                               = "terraform_ecs-service"
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
 
-  network_configuration {
+  ecs_service {
+    cluster_name = arn:aws:ecs:ap-south-1:077286633283:cluster/default
+    service_name = terraform_ecs
+  }
+
+network_configuration {
     assign_public_ip = "true"
     security_groups  = ["sg-113ea674"]
     subnets          = ["subnet-2fd9ae63", "subnet-39080951"]
@@ -28,9 +44,19 @@ resource "aws_ecs_service" "tfer--terraform_ecs_terraform_ecs-002D-service" {
   platform_version    = "LATEST"
   scheduling_strategy = "REPLICA"
 
-  tags = {
-    project = "newecs"
-  }
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [arn:aws:elasticloadbalancing:ap-south-1:077286633283:loadbalancer/app/my-lbs/c7d3c194fbdc5839]
+      }
 
-  task_definition = "arn:aws:ecs:ap-south-1:077286633283:task-definition/terraform_ecs-task:162"
+      target_group {
+        name = rule1.blue.name
+      }
+
+      target_group {
+        name = rule2.green.name
+      }
+    }
+  }
 }
